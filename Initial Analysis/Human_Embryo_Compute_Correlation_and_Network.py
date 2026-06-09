d8dd5918e210ba09fd28e2c7a9b40da84008122e
Rishabh Kumaran <rshbhkumaran@gmail.com>
2026-06-09 09:48:22 -0400

    Attempting to debug missing genes in bin 1 network

--- Initial Analysis/Human_Embryo_Compute_Correlation_and_Network.py
+++ Initial Analysis/Human_Embryo_Compute_Correlation_and_Network.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import anndata as ad
 import numpy as np
 import pandas as pd
@@ -7,20 +9,46 @@ import matplotlib.pyplot as plt
 import smoothie
 smoothie.suppress_warnings()
 
-sm_adata = ad.read_h5ad("../data/GSM9629357_CS23_E2S1_smooth_bin1.h5ad")
 
-print(sm_adata.shape)
+def compute_and_save_correlation(sm_adata, stem):
+    pearsonR_mat, p_val_mat = smoothie.compute_correlation_matrix(sm_adata.X)
+    np.save(f"../data/{stem}_pearsonR.npy", pearsonR_mat)
+    np.save(f"../data/{stem}_pval.npy", p_val_mat)
+    np.save(f"../data/{stem}_gene_names.npy", sm_adata.var_names.to_numpy())
+    return pearsonR_mat, p_val_mat
 
-pearsonR_mat, p_val_mat = smoothie.compute_correlation_matrix(sm_adata.X)
 
-np.save("../data/GSM9629357_CS23_E2S1_bin1_pearsonR.npy", pearsonR_mat)
-np.save("../data/GSM9629357_CS23_E2S1_bin1_pval.npy", p_val_mat)
-np.save("../data/GSM9629357_CS23_E2S1_bin_1gene_names.npy", sm_adata.var_names.to_numpy())
+def create_spatial_network(sm_adata, pearsonR_mat, pcc_cutoff=0.4, clustering_power=3, output_folder='../Cytoscape2'):
+    edge_list, node_label_df = smoothie.make_spatial_network(
+        pearsonR_mat=pearsonR_mat,  # don't change
+        gene_names=sm_adata.var_names,  # don't change
+        pcc_cutoff=pcc_cutoff,
+        clustering_power=clustering_power,
+        output_folder=output_folder
+    )
+    return edge_list, node_label_df
 
-edge_list, node_label_df = smoothie.make_spatial_network(
-    pearsonR_mat=pearsonR_mat, # don't change
-    gene_names=sm_adata.var_names, # don't change
-    pcc_cutoff=0.4,
-    clustering_power=3,
-    output_folder='../Cytoscape'
-)
+
+def plot_modules(sm_adata, node_label_df, output_folder='./module_plots', min_genes=3, spot_size=50, plots_per_row=10, dpi=300):
+    smoothie.plot_modules(
+        sm_adata,
+        node_label_df,
+        output_folder=output_folder,
+        min_genes=min_genes,
+        spot_size=spot_size,
+        plots_per_row=plots_per_row,
+        dpi=dpi
+    )
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    input_path = os.path.join("../data", filename)
+    stem = os.path.splitext(filename)[0]
+
+    sm_adata = ad.read_h5ad(input_path)
+    print(sm_adata.shape)
+
+    pearsonR_mat, p_val_mat = compute_and_save_correlation(sm_adata, stem)
+    edge_list, node_label_df = create_spatial_network(sm_adata, pearsonR_mat)
+    # plot_modules(sm_adata, node_label_df)
--- Initial Analysis/Human_Embryo_Raw_to_Smooth.py
+++ Initial Analysis/Human_Embryo_Raw_to_Smooth.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import anndata as ad
 import numpy as np
 import pandas as pd
@@ -7,7 +9,12 @@ import matplotlib.pyplot as plt
 import smoothie
 smoothie.suppress_warnings()
 
-sm_adata = adata = ad.read_h5ad("../data/GSM9629357_CS23_E2S1_raw_bin1.h5ad")
+filename = sys.argv[1]
+input_path = os.path.join("../data", filename)
+stem, ext = os.path.splitext(filename)
+output_path = os.path.join("../data", stem + "_smooth" + ext)
+
+adata = ad.read_h5ad(input_path)
 
 sc.pp.filter_cells(adata, min_counts=1)
 sc.pp.filter_genes(adata, min_counts=100)
@@ -17,13 +24,16 @@ sc.pp.log1p(adata)
 
 print(adata.shape)
 
-target_microns = 20.0
+target_microns = 40.0
 micron_to_unit_conversion = 2
 sm_adata = smoothie.run_parallelized_smoothing(
     adata,
     grid_based_or_not=True,
     gaussian_sd=target_microns * micron_to_unit_conversion,
-    min_spots_under_gaussian=25
+    min_spots_under_gaussian=1000
 )
 
-sm_adata.write_h5ad("../data/GSM9629357_CS23_E2S1_smooth_bin1.h5ad")
+print(f"Writing smoothed data to {output_path}")
+sm_adata.write_h5ad(output_path)
+
+print(sm_adata.shape)
